dfdc8cfeea19b0cd1f95990cd66f35a01fc975a4
Ubuntu <azureuser@sagar-linux.gh1hliohnzdupp0ayifraukqwb.bx.internal.cloudapp.net>
2026-07-06 14:02:39 +0000

    Update Terraform, Jenkins pipeline and Ansible deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,170 +1,222 @@
-resource "azurerm_resource_group" "test" {
-  name = "acctestrg"
-  location = "West US 2"
+terraform {
+  required_version = ">= 1.5"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.80"
+    }
+  }
 }
 
-resource "azurerm_virtual_network" "test" {
-  name = "acctvn"
+provider "azurerm" {
+  features {}
+
+  subscription_id = "3307e1c4-c1ce-4bcd-b25e-832db8e4c4f1"
+}
+
+############################
+# Resource Group
+############################
+
+resource "azurerm_resource_group" "rg" {
+  name     = "acctestrg"
+  location = "centralus"
+}
+
+############################
+# Virtual Network
+############################
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "acctvn"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
   address_space = [
-    "10.0.0.0/16"]
-  location = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
+    "10.0.0.0/16"
+  ]
 }
 
-resource "azurerm_subnet" "test" {
-  name = "acctsub"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  virtual_network_name = "${azurerm_virtual_network.test.name}"
-  address_prefix = "10.0.2.0/24"
+############################
+# Subnet
+############################
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "acctsub"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+
+  address_prefixes = [
+    "10.0.1.0/24"
+  ]
 }
 
-resource "azurerm_public_ip" "test" {
-  name = "publicIPForLB"
-  location = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  allocation_method       = "Static"
-  idle_timeout_in_minutes = 30
-  domain_name_label="dellemcappdemo"
-  tags {
+############################
+# Public IP
+############################
+
+resource "azurerm_public_ip" "pip" {
+  name                = "public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  allocation_method = "Static"
+  sku               = "Standard"
+
+  tags = {
     environment = "staging"
   }
 }
 
-resource "azurerm_network_security_group" "test" {
-  name                = "acceptanceTestSecurityGroup1"
-  location            = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  tags {
+############################
+# Network Security Group
+############################
+
+resource "azurerm_network_security_group" "nsg" {
+
+  name                = "demo-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  tags = {
     environment = "staging"
   }
 }
 
-resource "azurerm_network_security_rule" "test1" {
-  name                        = "SSH"
-  priority                    = 340
-  direction                   = "Inbound"
-  access                      = "Allow"
-  protocol                    = "*"
-  source_port_range           = "*"
-  destination_port_range      = "22"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = "${azurerm_resource_group.test.name}"
-  network_security_group_name = "${azurerm_network_security_group.test.name}"
-}
+############################
+# SSH Rule
+############################
 
-resource "azurerm_network_security_rule" "test2" {
-  name                        = "8080"
-  priority                    = 1020
+resource "azurerm_network_security_rule" "ssh" {
+
+  name                        = "AllowSSH"
+  priority                    = 100
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
+
   source_port_range           = "*"
-  destination_port_range      = "8080"
+  destination_port_range      = "22"
+
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
-  resource_group_name         = "${azurerm_resource_group.test.name}"
-  network_security_group_name = "${azurerm_network_security_group.test.name}"
+
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
 }
 
+############################
+# App Rule
+############################
 
+resource "azurerm_network_security_rule" "app8080" {
 
+  name                        = "Allow8080"
+  priority                    = 110
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
 
-resource "azurerm_network_interface" "test" {
-  count = 1
-  name = "acctni${count.index}"
-  location = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  network_security_group_id = "${azurerm_network_security_group.test.id}"
+  source_port_range           = "*"
+  destination_port_range      = "8080"
+
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+############################
+# Network Interface
+############################
+
+resource "azurerm_network_interface" "nic" {
+
+  name                = "demo-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
   ip_configuration {
-    name = "testConfiguration"
-    subnet_id = "${azurerm_subnet.test.id}"
-    private_ip_address_allocation = "dynamic"
-    public_ip_address_id = "${azurerm_public_ip.test.id}"
+
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
   }
+
+  depends_on = [
+    azurerm_subnet.subnet
+  ]
 }
 
-resource "azurerm_managed_disk" "test" {
-  count = 1
-  name = "datadisk_existing_${count.index}"
-  location = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  storage_account_type = "Standard_LRS"
-  create_option = "Empty"
-  disk_size_gb = "1023"
+############################
+# Associate NSG
+############################
+
+resource "azurerm_network_interface_security_group_association" "assoc" {
+
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
-resource "azurerm_availability_set" "avset" {
-  name = "avset"
-  location = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  platform_fault_domain_count = 2
-  platform_update_domain_count = 2
-  managed = true
-}
+############################
+# Linux VM
+############################
 
-resource "azurerm_virtual_machine" "test" {
-  count = 1
-  name = "dellemc_demo"
-  location = "${azurerm_resource_group.test.location}"
-  availability_set_id = "${azurerm_availability_set.avset.id}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  network_interface_ids = ["${azurerm_network_interface.test.id}"]
-  vm_size = "Standard_DS1_v2"
+resource "azurerm_linux_virtual_machine" "vm" {
 
-  # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
+  name                = "demo-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
 
-  # Uncomment this line to delete the data disks automatically when deleting the VM
-  # delete_data_disks_on_termination = true
+  size = "Standard_D2s_v3"
 
-  storage_image_reference {
+  admin_username = "azureuser"
+
+  disable_password_authentication = false
+
+  admin_password = "Password1234!"
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  os_disk {
+
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+
     publisher = "Canonical"
-    offer = "UbuntuServer"
-    sku = "16.04-LTS"
-    version = "latest"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku        = "22_04-lts"
+    version    = "latest"
   }
 
-  storage_os_disk {
-    name = "myosdisk${count.index}"
-    caching = "ReadWrite"
-    create_option = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-
-  # Optional data disks
-  storage_data_disk {
-    name = "datadisk_new_${count.index}"
-    managed_disk_type = "Standard_LRS"
-    create_option = "Empty"
-    lun = 0
-    disk_size_gb = "1023"
-  }
-
-  storage_data_disk {
-    name = "${element(azurerm_managed_disk.test.*.name, count.index)}"
-    managed_disk_id = "${element(azurerm_managed_disk.test.*.id, count.index)}"
-    create_option = "Attach"
-    lun = 1
-    disk_size_gb = "${element(azurerm_managed_disk.test.*.disk_size_gb, count.index)}"
-  }
-
-  os_profile {
-    computer_name = "dellemcdemovm"
-    admin_username = "testadmin"
-    admin_password = "Password1234!"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-
-  tags {
-    environment = "staging"
-  }
+  depends_on = [
+    azurerm_network_interface_security_group_association.assoc
+  ]
 }
 
+############################
+# Outputs
+############################
 
 output "public_ip_address" {
-  value = "${azurerm_public_ip.test.ip_address}"
+
+  value = azurerm_public_ip.pip.ip_address
+}
+
+output "vm_public_ip" {
+
+  value = azurerm_public_ip.pip.ip_address
+}
+
+output "ssh_command" {
+
+  value = "ssh azureuser@${azurerm_public_ip.pip.ip_address}"
 }
